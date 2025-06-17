f3a45a30dce1eb42145585faa2a38730de6832a3
Mia-FGB <92913584+Mia-FGB@users.noreply.github.com>
2025-06-17 11:12:52 +0100

    Uodating graphs for thesis

--- DNA_yield_graph.R
+++ DNA_yield_graph.R
@@ -1,27 +1,70 @@
+# R Script to plot DNA Yield data from the sampler comparison experiment 
+
+# packages 
 library(dplyr)
 library(ggplot2)
+library(patchwork)
+library(RColorBrewer)
+library(tidyr)
 library(patchwork)
 
 #read in data
 data <- read.csv("metadata/sampler_comparison_DNA_yield.csv")
 
-#adding a column for volume of air sampled
+# volume of air sampled
 data_air_vol <- data %>% 
   mutate(air_volume = Collection_time * Collection_rate) %>% 
-  #makind dna_yield numeric
-  mutate(DNA_yield = as.numeric(DNA_yield), genera_per_1000_reads = as.numeric(genera_per_1000_reads)) %>% 
-  #log the dna yield and air volume
+  # dna_yield numeric
+  mutate(DNA_yield = as.numeric(DNA_yield), 
+         genera_per_1000_reads = as.numeric(genera_per_1000_reads)) %>% 
+  # Replace NA values with 0.1 so they log okay
+  replace_na(list(
+    DNA_yield = 0.1,
+    genera_per_1000_reads = 0.1
+  )) %>%
+  # log the dna yield and air volume
   mutate(log_DNA_yield = log10(DNA_yield), log_air_volume = log10(air_volume),
          log_genera_per_1000_reads = log10(genera_per_1000_reads)) 
+  
+
+# Create Insect label col
+data_air_vol$Insect_Label <- ifelse(data_air_vol$Insect == "Yes", "*", NA)
+
+#Group data 7 remove those containing Insect 
+insect_remove_normalised <- subset(data_air_vol, !grepl("Yes", Insect)) %>% 
+  mutate(normalised_yield = DNA_yield/air_volume)
 
 
-#plot a scatter graph of DNA yield against volume of air sampled - simple
+# Theme for plots ------
+custom_theme <- theme_minimal(base_size = 12) +
+  theme(
+    axis.line = element_line(color = "black", linewidth = 0.3),
+    panel.grid.minor = element_blank(),
+  )
+
+# air_sampler_palette <- c(
+#   "InnovaPrep Bobcat" = "#FF7F50", 
+#   "Coriolis Compact" = "#008080", 
+#   "Coriolis Micro" = "#FFD700", 
+#   "InnovaPrep Cub" = "#8A2BE2", 
+#   "SASS 3100" = "#00CED1"
+# )
+
+air_sampler_palette <- setNames(brewer.pal(5, "Set2"), 
+                                c("InnovaPrep Bobcat", "Coriolis Compact", 
+                                  "Coriolis Micro", "InnovaPrep Cub", 
+                                  "SASS 3100"))
+
+
+# Plots ---------
+# Scatter graph of DNA yield against volume of air sampled 
 ggplot(data_air_vol, aes(x = air_volume, y= DNA_yield, colour = Air_Sampler)) +
-  geom_point() +theme_bw()
+  geom_point() + custom_theme
 
-#Normal graph -----------------
-#taking the normal data and then logging after so the axis remain the same
-ggplot(data_air_vol, aes(x = air_volume, y= DNA_yield)) +
+#DNA Yield against Air samples -----------------
+
+# Samplers are different shapes
+ ggplot(data_air_vol, aes(x = air_volume, y= DNA_yield)) +
   geom_point(aes(shape = Air_Sampler, colour = Location), size = 5) +
   #so colours match my other graphs
   scale_color_manual(values = c("#FF7F50", "#008080")) + 
@@ -29,32 +72,87 @@ ggplot(data_air_vol, aes(x = air_volume, y= DNA_yield)) +
   scale_shape_manual(values = c(19, 17, 15, 3, 4)) +
   theme_bw() +
   #Also find a way to highlight which samples contained insects
-  geom_text(aes(label = Insect, hjust = -0.25)) +
+  geom_text(aes(label = Insect_Label, hjust = -0.25, size = 5)) +
   #axis labels
   labs(x = "Log Volume of air sampled (L)", y = "Log DNA yield (ng)", title = "DNA yield (ng)") +
-  theme(plot.title = element_text(hjust = 0.5)) +
+  custom_theme +
   scale_y_log10() + scale_x_log10(labels = scales::comma)
 
-#Log data graph ---------------
-ggplot(data_air_vol, aes(x = log_air_volume, y= log_DNA_yield)) +
-  geom_point(aes(shape = Air_Sampler, colour = Location), size = 5) + theme_bw() +
-  #Also find a way to highlight which samples contained insects
-  geom_text(aes(label = Insect, hjust = -0.25)) +
-  #axis labels
-  labs(x = "Log Volume of air sampled (L)", y = "Log DNA yield (ng)", title = "DNA yield (ng)") +
-  theme(plot.title = element_text(hjust = 0.5))
 
-#--------------------------------------------------------------
+# Samplers diff colours ----
+dna_yield <- ggplot(data_air_vol, aes(x = air_volume, y = DNA_yield)) +
+  
+  # Translucent points with coloured border
+  geom_point(aes(fill = Air_Sampler, shape = Location, colour = Air_Sampler),
+             size = 5, alpha = 0.7, stroke = 1) +
+  
+  # Manual fill and border colour palette
+  scale_fill_manual(name = "Air Sampler", values = air_sampler_palette) +
+  
+  # Add visible asterisk where insect was present (no legend here)
+  geom_text(data = subset(data_air_vol, Insect == "Yes"),
+            aes(label = Insect_Label),
+            size = 8, hjust = -0.4, show.legend = FALSE) +
+  
+  # Colour and shape scales
+  scale_color_manual(name = "Air Sampler", values = air_sampler_palette) +
+  scale_shape_manual(name = "Location", values = c(
+    "Church Farm" = 21, "NHM" = 22, "Yes" = 8),
+    labels = c("Church Farm", "NHM")) +
+  
+  # Labels and theme
+  labs(x = "Volume of air sampled (L)", 
+       y = "DNA yield (ng)",
+       colour = "Air Sampler") +
+  custom_theme +
+  # scale_y_log10() +
+  # scale_x_log10(labels = scales::comma)
+  
+dna_yield
+
+no_insect_dna_yield <- ggplot(insect_remove_normalised, aes(x = air_volume, y = DNA_yield)) +
+  
+  # Translucent points with coloured border
+  geom_point(aes(fill = Air_Sampler, shape = Location, colour = Air_Sampler),
+             size = 5, alpha = 0.7, stroke = 1) +
+  
+  # Manual fill and border colour palette
+  scale_fill_manual(name = "Air Sampler", values = air_sampler_palette) +
+  
+  # Colour and shape scales
+  scale_color_manual(name = "Air Sampler", values = air_sampler_palette) +
+  scale_shape_manual(name = "Location", values = c(
+    "Church Farm" = 21, "NHM" = 22, "Yes" = 8),
+    labels = c("Church Farm", "NHM")) +
+  
+  # Labels and theme
+  labs(x = "Volume of air sampled (L)", 
+       y = "DNA yield (ng)",
+       colour = "Air Sampler") +
+  custom_theme 
+
+no_insect_dna_yield
+
+#Combine plots
+com_dna_yield <-  dna_yield + no_insect_dna_yield +
+  plot_layout(guides = "collect") &
+  theme(legend.position = "right")
+
+# Save plots
+ggsave("images/DNA_yield/no_inse_yield_air_vol.pdf", no_insect_dna_yield, width=10, height=6, dpi = 300 )
+ggsave("images/DNA_yield/yield_air_vol.pdf", dna_yield, width=10, height=6, dpi = 300 )
+ggsave("images/DNA_yield/Comb_DNA_yield_scatter.pdf", com_dna_yield, width=12, height=6, dpi = 300 )
+
+
 #Plotting genera count per 1000 reads----------------------
-#not sure if since it's for 1000 reads it's considered normalised?
-#--------------------------------------------------------------
-#plot a scatter graph of genera count against volume of air sampled - simple
+
+#Simple graph
 ggplot(data_air_vol, aes(x = air_volume, y= genera_per_1000_reads, colour = Air_Sampler)) +
   geom_point(aes(shape = Air_Sampler, colour = Location), size = 5) + theme_bw() 
 
-#need to log the axis for better seperation 
-#Log genera count  graph ---------------
-ggplot(data_air_vol, aes(x = log_air_volume, y= genera_per_1000_reads)) +
+#Log genera count  graph 
+# sampler = shape, location = colour
+ggplot(data_air_vol, aes(x = air_volume, y= genera_per_1000_reads)) +
   geom_point(aes(shape = Air_Sampler, colour = Location), size = 5) + 
   #so colours match my other graphs
   scale_color_manual(values = c("#FF7F50", "#008080")) + 
@@ -64,44 +162,82 @@ ggplot(data_air_vol, aes(x = log_air_volume, y= genera_per_1000_reads)) +
   #Also find a way to highlight which samples contained insects
   #geom_text(aes(label = Insect, hjust = -0.25)) +
   #axis labels
-  labs(x = "Log Volume of air sampled (L)", y = "Genera per 1000 reads", title = "Diversity accumulation") +
+  labs(x = "Volume of air sampled (L)", y = "Genera per 1000 reads", title = "Diversity accumulation") +
   theme(plot.title = element_text(hjust = 0.5))
 
-#--------------------------------------------------------------
-#Trying to plot a graph of averaged data----------------------
-#--------------------------------------------------------------
-
-#Remove nd's and read the DNA_yield variable as numeric
-clean_data <- data_air_vol %>% 
-  #need to remove any nd's
-  filter(DNA_yield != "nd") %>% 
-  #makind dna_yield numeric
-  mutate(DNA_yield = as.numeric(DNA_yield))
-
-#check the strucutre of the dataset
-str(clean_data)
 
 
-#grouping the data - removing those with insect 
-insect_remove_normalised <- subset(data_air_vol, !grepl("Insect", Insect)) %>% 
-  filter(DNA_yield != "nd") %>% 
-  mutate(normalised_yield = DNA_yield/air_volume)
+# Thesis ready graph - sampler = colour, location = shape
+genera_plot <- ggplot(data_air_vol, aes(x = air_volume, y = genera_per_1000_reads)) +
+  
+  # Translucent points with coloured border
+  geom_point(aes(fill = Air_Sampler, shape = Location, colour = Air_Sampler),
+             size = 5, alpha = 0.7, stroke = 1) +
+  
+  # Manual fill and border colour palette
+  scale_fill_manual(name = "Air Sampler", values = air_sampler_palette) +
+  
+  # Add visible asterisk where insect was present (no legend here)
+  geom_text(data = subset(data_air_vol, Insect == "Yes"),
+            aes(label = Insect_Label),
+            size = 8, hjust = -0.4, show.legend = FALSE) +
+  
+  # Colour and shape scales
+  scale_color_manual(name = "Air Sampler", values = air_sampler_palette) +
+  scale_shape_manual(name = "Location", values = c(
+    "Church Farm" = 21, "NHM" = 22, "Yes" = 8),
+    labels = c("Church Farm", "NHM")) +
+  
+  # Labels and theme
+  labs(x = "Volume of air sampled (L)", 
+       y = "Genera per 1000 reads",
+       colour = "Air Sampler") +
+  
+  custom_theme
+  # + scale_x_log10(labels = scales::comma)
+
+ggsave("images/DNA_yield/genera_per_1k_air.pdf", genera_plot, width=10, height=6, dpi = 300 )
 
 
-#Boxplot
+#Boxplots ----------------------
+
+#Boxplot- not considering location 
 yield_boxplot <- ggplot(insect_remove_normalised, aes(x=Air_Sampler, y = DNA_yield)) +
-  geom_boxplot() + theme_bw() +
-  labs(y = "DNA yield (ng)", x = "Air Sampler") 
+  geom_boxplot(aes(fill = Air_Sampler), colour = "black" , alpha = 0.7) + 
+  scale_fill_manual(name = "Air Sampler", values = air_sampler_palette) +
+  custom_theme +
+  labs(y = "DNA yield (ng)", x = "Air Sampler") +
+  scale_x_discrete(labels = c(
+    "Coriolis Compact" = "Coriolis\nCompact",
+    "Coriolis Micro" = "Coriolis\nMicro",
+    "InnovaPrep Bobcat" = "InnovaPrep\nBobcat",
+    "InnovaPrep Cub" = "InnovaPrep\nCub",
+    "SASS 3100" = "SASS\n3100"
+  ))
 
+# DNA yield normalised by air volume
 norm_yield_boxplot <- ggplot(insect_remove_normalised, aes(x=Air_Sampler, y = normalised_yield)) +
-  geom_boxplot() + theme_bw() +
-  labs(y = " Normalised DNA yield (ng)", x = "Air Sampler") 
+  geom_boxplot(aes(fill = Air_Sampler), colour = "black" , alpha = 0.7) + 
+  scale_fill_manual(name = "Air Sampler", values = air_sampler_palette) +
+  custom_theme +
+  labs(y = " Normalised DNA yield (ng/L)", x = "Air Sampler") +
+  scale_x_discrete(labels = c(
+    "Coriolis Compact" = "Coriolis\nCompact",
+    "Coriolis Micro" = "Coriolis\nMicro",
+    "InnovaPrep Bobcat" = "InnovaPrep\nBobcat",
+    "InnovaPrep Cub" = "InnovaPrep\nCub",
+    "SASS 3100" = "SASS\n3100"
+  ))
 
-comb_boxplot <- yield_boxplot + norm_yield_boxplot
+comb_boxplot <- yield_boxplot + norm_yield_boxplot +
+  plot_layout(guides = "collect") &
+  theme(legend.position = "right")
 
-ggsave("images/DNA_yield_boxplot.pdf", yield_boxplot,width=6, height=6, dpi = 300 )
-ggsave("images/Norm_DNA_yield_boxplot.pdf", norm_yield_boxplot,width=6, height=6, dpi = 300 )
-ggsave("images/Comb_DNA_yield_boxplot.pdf", comb_boxplot,width=12, height=6, dpi = 300 )
+comb_boxplot
+
+ggsave("images/DNA_yield/DNA_yield_boxplot.pdf", yield_boxplot,width=6, height=6, dpi = 300 )
+ggsave("images/DNA_yield/Norm_DNA_yield_boxplot.pdf", norm_yield_boxplot,width=6, height=6, dpi = 300 )
+ggsave("images/DNA_yield/Comb_DNA_yield_boxplot.pdf", comb_boxplot,width=14, height=6, dpi = 300 )
 
 
 
--- Marti_MEGAN_comparison_visualisation.R
+++ Marti_MEGAN_comparison_visualisation.R
@@ -2,14 +2,37 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 library(reshape2)
+library(viridis)
 
-data <- read.csv("read_data/Marti_megan_comparison_barcode-14/marti_megan_read_counts_barcode14.csv")
+#Script for MARTi and MEGAN comparison
+# notes here - https://docs.google.com/document/d/1KHRCVzLtTlWvmLMS4uyc59MQRDNI5T8OwjvozabSrfc/edit?tab=t.0
+#  Updated this June 2025
 
+custom_theme <- theme_minimal(base_size = 12) +
+  theme(
+    axis.line = element_line(color = "black", linewidth = 0.3),
+    panel.grid.minor = element_blank(),
+  )
+
+data <- read.csv("Marti_megan_comparison_barcode-14/marti_megan_read_counts_barcode14.csv")
+
+
+# Filter the data to remove assigned read counts and just use summarised
+data_filtered <- data[, !grepl("As", colnames(data))]
+
+#Rename the data
+colnames(data_filtered) <- gsub("_Su$", "", colnames(data_filtered))  # remove _Su at the end
+colnames(data_filtered) <- gsub("^ME$", "MEGAN", colnames(data_filtered))  # rename MEGAN
 
 #need to reshape the data
-data_long <- data %>% 
-  pivot_longer(cols = -Species, names_to = "Software", values_to = "Read_Count") %>% 
+data_long <- data_filtered %>% 
+  pivot_longer(cols = -Species, names_to = "RunName", values_to = "Read_Count") %>% 
   filter(Species != "Unclassified")
+
+#Custim order
+custom_order <- c("MEGAN", "MA_LCA_100_MI_60", "MA_LCA_100_MI_70", "MA_LCA_20_MI_60", "MA_LCA_20_MI_70")
+data_long$RunName <- factor(data_long$RunName, levels = custom_order)
+
 
 #Calculate the average read counts for each species
 species_averages <- data_long %>%
@@ -17,7 +40,7 @@ species_averages <- data_long %>%
   summarise(Average_Read_Count = mean(Read_Count))
 
 # Order the species based on the highest average read count
-#don't want this for heatmap
+# don't want this for heatmap
 ordered_species <- species_averages %>%
   arrange(desc(Average_Read_Count)) %>%
   pull(Species)
@@ -26,39 +49,48 @@ ordered_species <- species_averages %>%
 data_long$Species <- factor(data_long$Species, levels = ordered_species)
 
 # Define custom color palette
-my_colors <- c("#449C75", "#5AE8A9", "#25509C", "#437EE8", "#B5713E", "#ECBC97", "#8B57A1", "#DBAFED"
-               , "#ED9151", "#F7AF54")
-
+my_colors <- c("#449C75", "#B5713E", "#ECBC97", "#25509C", "#437EE8") 
 
 #barplot for each species, ordered by read count
-ggplot(data_long, aes(x = Species, y = Read_Count, fill = Software)) +
+ggplot(data_long, aes(x = Species, y = Read_Count, fill = RunName)) +
   geom_bar(stat = "identity", position = "dodge") +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
-  scale_fill_manual(values = my_colors, name = "Software")
+  scale_fill_manual(values = my_colors, name = "RunName") +
+  custom_theme +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 1)) 
+
+# Plotting just top 15 species
+top_species <- data_long %>%
+  group_by(Species) %>%
+  summarise(total_reads = sum(Read_Count)) %>%
+  top_n(15, total_reads) %>%
+  pull(Species)
+
+top_15 <- data_long %>% filter(Species %in% top_species)
+
+top_15_plot <- ggplot(top_15, aes(x = Species, y = Read_Count, fill = RunName)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_fill_manual(values = my_colors, name = "RunName") +
+  custom_theme +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 1))  +
+  labs(
+    y = "Read Count",
+  )
   
-
-#Trying other graphs to better show the parameters 
-#logging the data for better spread
-data_long$log_Read_Count <- log(data_long$Read_Count + 1)
-
-data_long$Software <- factor(data_long$Software ,
-                             levels = c("MA_LCA_20_MI_70_As", "MA_LCA_20_MI_70_Su","MA_LCA_20_MI_60_As", "MA_LCA_20_MI_60_Su", 
-                                        "MA_LCA_100_MI_70_As", "MA_LCA_100_MI_70_Su", "MA_LCA_100_MI_60_As", "MA_LCA_100_MI_60_Su", 
-                                        "ME_As", "ME_Su"))
+ggsave("images/marti_megan_comparison/top15_readcount.pdf", top_15_plot, width=8, height=6, dpi = 300 )
 
 #Heatmap
-#think about how to group the data along the y axis
-ggplot(data_long, aes(x = Software, y = Species, fill = log_Read_Count)) +
+heatmap <- ggplot(data_long, aes(x = RunName, y = Species, fill = Read_Count)) +
   geom_tile() +
-  scale_fill_gradient(low = "white", high = "#449C75") +
-  labs(title = "Metagenomic Taxonomic Classification",
-       x = "Software and Thresholds",
-       y = "Species") +
-  theme_bw() +
+  scale_fill_viridis_c(
+    trans = "log1p",  # log1p handles 0 safely (log1p(x) = log(1 + x))
+    name = "Read Count (log)",
+    breaks = c(1, 10, 100, 1000, 10000),
+    labels = scales::comma_format()(c(1, 10, 100, 1000, 10000)),
+    option = "D"  # Must be a string, e.g. "D" not just D
+  ) +
+  custom_theme +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-
-
+ggsave("images/marti_megan_comparison/heatmap_0625.pdf", heatmap, width=8, height=6, dpi = 300 )
 
 
